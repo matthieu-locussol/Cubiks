a73fdf7a70ffbd3b11989c4e6bf0c12ae74aea71
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-05-04 16:45:11 +0200

    feat: working cube texturing

--- application/main.cpp
+++ application/main.cpp
@@ -1,12 +1,29 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <SFML/Window.hpp>
 #include <glbinding/gl/gl.h>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+#include <globjects/Texture.h>
 #include <globjects/VertexAttributeBinding.h>
 #include <globjects/base/File.h>
-#include <globjects/Texture.h>
 #include <globjects/globjects.h>
+#include <iostream>
+
+#include <Camera.hpp>
+
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = WINDOW_WIDTH / 2.0f;
+float lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouse = true;
+float delta = 0.0f;
+float lastFrame = 0.0f;
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +35,7 @@ int main(int argc, char* argv[])
     settings.minorVersion = 3;
     settings.attributeFlags = sf::ContextSettings::Core;
 
-    sf::Window window(sf::VideoMode(1200, 800), "Cubiks", sf::Style::Default,
+    sf::Window window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Cubiks", sf::Style::Default,
         settings);
     window.setVerticalSyncEnabled(true);
     window.setActive(true);
@@ -38,50 +55,55 @@ int main(int argc, char* argv[])
     program->attach(vertexShader.get(), fragmentShader.get());
 
     const std::array<float, 180> vertices{
+        // TOP FACE
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        // BOTTOM FACE
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        // FRONT FACE
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-         0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-         0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-
+        // BACK FACE
         -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-         0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-         0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-         0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
         -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
         -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-
+        // RIGHT FACE
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        // LEFT FACE
         -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
         -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
         -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
         -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
         -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f};
 
-         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-         0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-         0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-         0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-         0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-         0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
-    };
+    const std::array<int, 6> indices = {
+        0, 1, 3,
+        1, 2, 3};
 
     auto buffer = globjects::Buffer::create();
+    buffer->bind(gl::GL_ARRAY_BUFFER);
     buffer->setData(vertices, gl::GL_STATIC_DRAW);
 
     auto vao = globjects::VertexArray::create();
@@ -93,40 +115,90 @@ int main(int argc, char* argv[])
 
     auto textureAttribute = vao->binding(1);
     textureAttribute->setAttribute(1);
-    textureAttribute->setBuffer(buffer.get(), 0, sizeof(glm::vec3));
+    textureAttribute->setBuffer(buffer.get(), 0, 5 * sizeof(float));
     textureAttribute->setFormat(2, gl::GL_FLOAT, gl::GL_FALSE, 3 * sizeof(float));
 
     vao->enable(0);
     vao->enable(1);
 
-
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("assets/cube.png", &width, &height, &nrChannels, 0);
 
     auto texture = globjects::Texture::create(gl::GL_TEXTURE_2D);
-    texture->setParameter(gl::GL_TEXTURE_MIN_FILTER, gl::GL_LINEAR);
-    texture->setParameter(gl::GL_TEXTURE_MAG_FILTER, gl::GL_LINEAR);
-    texture->setParameter(gl::GL_TEXTURE_WRAP_S, gl::GL_CLAMP_TO_EDGE);
-    texture->setParameter(gl::GL_TEXTURE_WRAP_T, gl::GL_CLAMP_TO_EDGE);
-
-    texture->image2D(0, gl::GL_RGBA8, glm::ivec2(512), 0, gl::GL_RGBA, gl::GL_UNSIGNED_BYTE, nullptr);
-    texture->clearImage(0, gl::GL_RGBA, gl::GL_UNSIGNED_BYTE, glm::ivec4(255, 255, 255, 255));
+    texture->setParameter(gl::GL_TEXTURE_MIN_FILTER, gl::GL_NEAREST);
+    texture->setParameter(gl::GL_TEXTURE_MAG_FILTER, gl::GL_NEAREST);
+    texture->image2D(0, gl::GL_RGBA, glm::ivec2(width, height), 0, gl::GL_RGBA, gl::GL_UNSIGNED_BYTE, data);
     texture->generateMipmap();
 
+    stbi_image_free(data);
 
+    gl::glEnable(gl::GL_DEPTH_TEST);
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
+        float currentFrame = clock.getElapsedTime().asSeconds();
+        delta = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         sf::Event event;
 
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            else if (event.type == sf::Event::MouseMoved) {
+                if (firstMouse) {
+                    lastX = event.mouseMove.x;
+                    lastY = event.mouseMove.y;
+                    firstMouse = false;
+                }
+
+                glm::vec2 offset(event.mouseMove.x - lastX, lastY - event.mouseMove.y);
+
+                lastX = event.mouseMove.x;
+                lastY = event.mouseMove.y;
+
+                camera.lookCamera(offset, false);
+            }
+            else if (event.type == sf::Event::MouseWheelMoved) {
+                glm::vec2 offset(event.mouseWheel.delta, event.mouseWheel.delta);
+                camera.zoomCamera(offset);
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Z) {
+                    camera.moveCamera(Camera::FORWARD, delta);
+                }
+                else if (event.key.code == sf::Keyboard::Q) {
+                    camera.moveCamera(Camera::LEFT, delta);
+                }
+                else if (event.key.code == sf::Keyboard::S) {
+                    camera.moveCamera(Camera::BACKWARD, delta);
+                }
+                else if (event.key.code == sf::Keyboard::D) {
+                    camera.moveCamera(Camera::RIGHT, delta);
+                }
+                else if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
         }
 
         gl::glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT | gl::GL_DEPTH_BUFFER_BIT);
 
         program->use();
-        vao->drawArrays(gl::GL_TRIANGLE_STRIP, 0, 3);
+
+        glm::mat4 view = camera.getViewMatrix();
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.getZoom()), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
+
+        program->setUniform("view", view);
+        program->setUniform("model", model);
+        program->setUniform("projection", projection);
+
+        vao->drawArrays(gl::GL_TRIANGLES, 0, 36);
+
         program->release();
 
         window.display();
