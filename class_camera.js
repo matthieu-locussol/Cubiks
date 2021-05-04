35ab1d6f1fd2d90a642581d4f30c960c5b68c2f7
matthieu-locussol <matthieu-locussol@users.noreply.github.com>
2021-05-04 15:26:46 +0000

    deploy: a73fdf7a70ffbd3b11989c4e6bf0c12ae74aea71

--- class_camera.js
+++ class_camera.js
@@ -1,9 +1,20 @@
 var class_camera =
 [
-    [ "Camera", "class_camera.html#ae23af4d7cab430c77d537621cdd16b3f", null ],
+    [ "Movement", "class_camera.html#a00ab03c6d8703e48a91b13e363a0958e", [
+      [ "FORWARD", "class_camera.html#a00ab03c6d8703e48a91b13e363a0958ea6388b5408cf8440021383388044ae77f", null ],
+      [ "BACKWARD", "class_camera.html#a00ab03c6d8703e48a91b13e363a0958eae18db08a2289896f26dcddf6e2ad274f", null ],
+      [ "LEFT", "class_camera.html#a00ab03c6d8703e48a91b13e363a0958ea1bed5588ea4c26163a72f0fc8621f6be", null ],
+      [ "RIGHT", "class_camera.html#a00ab03c6d8703e48a91b13e363a0958eaedac3a8506bf2bae7df2d36dd5580884", null ]
+    ] ],
+    [ "Camera", "class_camera.html#a1ad8bd489971e8f2279fe2d410bcc4d9", null ],
     [ "Camera", "class_camera.html#a32200bd57f6c4d7d6dce8d9bac7a7724", null ],
     [ "Camera", "class_camera.html#a18cea16e830732cc106c27c7a840c3ac", null ],
     [ "~Camera", "class_camera.html#adcb96efefa7af58e3ee6534b15d4979b", null ],
+    [ "getViewMatrix", "class_camera.html#a2f90e661a78172675ca06ea69667b531", null ],
+    [ "getZoom", "class_camera.html#a811613828473f845ff5ab6b38a3492a9", null ],
+    [ "lookCamera", "class_camera.html#abd1603faacd2575bac3618c65d0335d4", null ],
+    [ "moveCamera", "class_camera.html#a12de7596a0e8cb50d8602674c71e04d1", null ],
     [ "operator=", "class_camera.html#a569422f7adefb235e98cbc8ed5bcf518", null ],
-    [ "operator=", "class_camera.html#aaa8ba4f5a95a179f86fc534a40af3766", null ]
+    [ "operator=", "class_camera.html#aaa8ba4f5a95a179f86fc534a40af3766", null ],
+    [ "zoomCamera", "class_camera.html#a8212bbf2a24c8496c22de183b08ab3c6", null ]
 ];
